c8ddf30ab893e03d18057bcde5ec9f2ba1ab72f8
fayiskooni <fayiskooni@gmail.com>
2025-10-30 15:17:29 +0530

    root path condition changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:5001/api/auth" : "/api/auth";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api/auth"
+    : "/api/auth";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
