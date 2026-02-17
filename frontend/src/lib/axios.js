7e9e271f2c25f39ca3db964c905106ab457eb0bd
fayiskooni <fayiskooni@gmail.com>
2026-02-17 19:50:30 +0530

    backend url added in env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+console.log(BACKEND_URL, "BACKEND_URL");
+console.log(BACKEND_URL, "BACKEND_URL");
+
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api/auth"
-    : "/api/auth";
+    ? BACKEND_URL || "http://localhost:5001/api/auth"
+    : BACKEND_URL;
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), cloudflare()],
+//   define: {
+//     BACKEND_URL: JSON.stringify(env.VITE_BACKEND_URL),
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react(), cloudflare()],
+    define: {
+      BACKEND_URL: JSON.stringify(env.VITE_BACKEND_URL),
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  })
+}
